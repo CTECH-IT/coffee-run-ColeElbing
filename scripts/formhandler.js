9d7031237c0cc7b6a91940fdbcece06aad970d08
ColeElbing <8517401@isd535.org>
2022-03-18 10:27:35 -0500

    created the formhandler and uploaded jQuery

---
+++ scripts/formhandler.js
@@ -0,0 +1,13 @@
+(function(window){
+    'use strict';
+
+    let App = window.App || {};
+
+    function FormHandler(){
+        //code
+    }
+
+    App = window.App;
+    App.FormHandler = FormHandler;
+
+}(window));
