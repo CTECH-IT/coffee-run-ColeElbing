fa77bb5724b9cfa7d294028f89ddba7a724592f4
ColeElbing <8517401@isd535.org>
2022-03-18 11:30:44 -0500

    Fixed the constructor issue, whenever i add more to the code inorder to create the submit handler the issue reappears

--- scripts/formhandler.js
+++ scripts/formhandler.js
@@ -10,6 +10,23 @@
         }
     }
 
+    
+    //find the "selector" in the DOM using jQuery and assign it to this.formElement
+    //this.$formElement = $(selector);
+    //if (this.$formElement.length == 0){
+    //   throw new Error('Could not find element with selector: ' + selector);
+    //}
+
+    //FormHandler.prototype.addSubmitHandler = function() {
+    //   console.log('Setting the submit handler for the form...');
+    //    this.$formElement.on('submit', function(event){
+    //        event.preventDefault();
+
+    //        let data = $(this).serializeArray();
+    //        console.log(data);
+    //});
+    //}
+
     App.FormHandler = FormHandler;
     window.App = App;
 
