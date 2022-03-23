5452ac6f087026282a50cf395c00d2296833341c
ColeElbing <8517401@isd535.org>
2022-03-23 11:33:29 -0500

    Created the checklist and completed everything up to add the click handler due to error

---
+++ scripts/checklist.js
@@ -0,0 +1,77 @@
+(function (window) {
+    'use strict'
+    let App = window.App || {};
+    let $ = window.jQuery;
+
+    function CheckList(selector) {
+        if (!selector) {
+            throw new Error('No selector provided');
+        }
+
+        this.$element = $(selector);
+        if (this.$element.length == 0) {
+            throw new Error('Could not find element with selector: ' + selector);
+        }
+    }
+
+    // the method that adds a new row to the checklist
+    CheckList.prototype.addRow = function (coffeeOrder) {
+        // Remove any existing rows that match the email address
+        this.removeRow(coffeeOrder.emailAddress);
+        // Create a new instance of a row, using the coffee order info
+        var rowElement = new Row(coffeeOrder);
+        // add the new row instance's $element property to the checklist
+        this.$element.append(rowElement.$element);
+    };
+
+    // when the checkbox is clicked, get the email address from the row
+    // and the call the function (func) that is passed in with the email as a parameter
+
+    CheckList.prototype.addClickHandler = function(func) {
+        this.$element.on('click','input', function(event) {
+            var email = event.target.value;
+            this.removeRow(email);
+            func(email);
+        }.bind(this));
+    };
+
+    // remove a row identified by an email address
+    CheckList.prototype.removeRow = function (email) {
+        this.$element
+            .find('[value="' + email + '"]')
+            .closest('[data-coffee-order = "checkbox"]')
+            .remove();
+    };
+
+    // Each row is one oustanding order
+    function Row(coffeeOrder) {
+        let $div = $('<div></div>', {
+            'data-coffee-order': 'checkbox',
+            'class': 'checkbox'
+        });
+        let $label = $('<label></label>');
+
+        let $checkbox = $('<input></input>', {
+            type: 'checkbox',
+            value: coffeeOrder.emailAddress
+        });
+
+        let description = coffeeOrder.size + ' ';
+        if (coffeeOrder.flavor) {
+            description += coffeeOrder.flavor + ' ';
+        }
+        description += coffeeOrder.coffee + ' ';
+        description += ' (' + coffeeOrder.emailAddress + ')';
+        description += ' [' + coffeeOrder.strength + 'x]';
+
+        $label.append($checkbox);
+        $label.append(description);
+        $div.append($label);
+
+        this.$element = $div;
+    }
+
+    // Add the checklist to the App namespace
+    App.CheckList = CheckList;
+    window.App = App;
+})(window);
